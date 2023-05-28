527e54e417a7b194f3a80dc234dfd4aa8ce29568
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-05-28 11:03:43 -0400

    update

--- src/components/HeaderFolder/CTA.jsx
+++ src/components/HeaderFolder/CTA.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import Resume from '../../assets/resume.pdf'
+import Resume from '../../assets/resumeJeffBozier.pdf'
 
 const CTA = () => {
+
+    // show resume on button press
+    // show div of pic of resume 
+    // div is fixed to screen
+    // grays out everything behind it 
+    // exit button 
+    // download button 
+    // download functions 
+
   return (
     <div className='cta'>
-        <a href={Resume} download className='btn btn-primary'>Download CV</a>
+        <a id='resumePic' href={Resume} download className='btn btn-primary'>Download Resume</a>
+        <a id='resumePic' href={Resume} className='btn btn-primary'>View Resume</a>
         <a href="#contact" className='btn-primary btn'>Let's Talk</a>
     </div>
   )
