26b9dbf77398637bec01787c28f7509aa9cbd8d8
Jeff B <jeff.bozierjr@gmail.com>
2024-04-16 11:38:19 -0400

    updating

--- src/components/PortfolioFolder/Portfolio.jsx
+++ src/components/PortfolioFolder/Portfolio.jsx
@@ -2,18 +2,39 @@ import React, { useEffect } from 'react'
 import './Portfolio.css'
 import imgFitness from "../../assets/personalTraining.png"
 import imgstambrose from "../../assets/stambrose.png"
-import img1 from '../../assets/gamecornerA.png'
+// import img1 from '../../assets/gamecornerA.png'
 import img2 from '../../assets/truckingSite.png'
-import img3 from '../../assets/teacherCalcA.png'
-import img4 from '../../assets/dashboard.png'
-import img5 from '../../assets/DateListTracker.png'
+// import img3 from '../../assets/teacherCalcA.png'
+// import img4 from '../../assets/dashboard.png'
+// import img5 from '../../assets/DateListTracker.png'
 import img6 from '../../assets/originalPortfolio.png'
+import img7 from '../../assets/img7.png'
 import { animateOnScroll } from '../../Functions/animateScrollFunctions'
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaDatabase, FaNodeJs, FaServer, FaJava } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaDatabase, FaNodeJs, FaServer } from 'react-icons/fa';
+import { SiMinutemailer } from "react-icons/si";
+
 
 const data = [
   {
     id: 1, 
+    image: img7,
+    tools:
+    ['HTML5',
+    <FaHtml5 key="html5" />,
+    'CSS3',
+    <FaCss3Alt key="css3" />,
+    'JavaScript',
+    <FaJs key="javascript" />,
+    'Emailjs',
+    <SiMinutemailer key="emailJs"/>
+    ],
+
+    title: "Bozier Wedding Website",
+    github: "https://github.com/Jeff-Shango/bobawedding",
+    demo: "https://bozierwedding.netlify.app"
+  },
+  {
+    id: 3, 
     image: imgFitness,
     tools: [
       'HTML5',
@@ -60,21 +81,6 @@ const data = [
     demo: "https://clientlookup.netlify.app"
   },
   {
-    id: 3, 
-    image: img1,
-    tools:
-    ['HTML5',
-    <FaHtml5 key="html5" />,
-    'CSS3',
-    <FaCss3Alt key="css3" />,
-    'JavaScript',
-    <FaJs key="javascript" />,
-    ],
-    title: "Shango's Game Corner",
-    github: "https://github.com/Jeff-Shango/word-game.git",
-    demo: "https://github.com/Jeff-Shango/word-game.git"
-  },
-  {
     id: 4, 
     image: img2,
     tools:
@@ -84,62 +90,11 @@ const data = [
     <FaCss3Alt key="css3" />,
     'JavaScript',
     <FaJs key="javascript" />,
+    
     ],
     title: "Only Wright Trucking Site",
     github: "https://github.com/Jeff-Shango/OnlyWrightTraining.git",
     demo: "https://onlywrighttrucking.netlify.app"
-  },
-  {
-    id: 5, 
-    image: img3,
-    tools:
-    ['HTML5',
-    <FaHtml5 key="html5" />,
-    'CSS3',
-    <FaCss3Alt key="css3" />,
-    'JavaScript',
-    <FaJs key="javascript" />,
-    'Java',
-    <FaJava key='Java'/>],
-    title: "Teacher Grade Tracker",
-    github: "https://github.com/Jeff-Shango/GradeTracker.git",
-    demo: "https://github.com/Jeff-Shango/GradeTracker.git"
-  },
-  {
-    id: 6, 
-    image: img4,
-    tools:
-    ['HTML5',
-    <FaHtml5 key="html5" />,
-    'CSS3',
-    <FaCss3Alt key="css3" />,
-    'JavaScript',
-    <FaJs key="javascript" />,
-    'React',
-    <FaReact key="react" />,
-    'MySQL',
-    <FaDatabase key="mysql" />,
-    'Node.js',
-    <FaNodeJs key="nodejs" />,
-    'Express.js',
-    <FaServer key="expressjs" />,],
-    title: "Dashboard using React",
-    github: "https://github.com/Jeff-Shango/dashboard.git",
-    demo: "https://github.com/Jeff-Shango/dashboard.git"
-  },
-  {
-    id: 7, 
-    image: img5,
-    tools:
-    ['HTML5',
-    <FaHtml5 key="html5" />,
-    'CSS3',
-    <FaCss3Alt key="css3" />,
-    'JavaScript',
-    <FaJs key="javascript" />],
-    title: "Date Night List Tracker",
-    github: "https://github.com/Jeff-Shango/todoapp.git",
-    demo: "https://github.com/Jeff-Shango/todoapp.git"
   },
   {
     id: 8, 
